d1f29013ce0597c583fd80a51c1dfe229eb8c38f
Jon Schwenn <jonschwenn@gmail.com>
2013-11-08 08:49:50 -0600

    Tweak to new/redeploy HD discovery
    Depending on if it's a blank Virtual HD or already partitioned, awk will hit on the disk designation on the 4th or 5th column.  Also may hit on multiple lines if already partitioned.  'head -1' grabs the first line and the awk command is changed to pull the last field.

--- scripts/vm_disk_partition.sh
+++ scripts/vm_disk_partition.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-GETDISK=`diskutil list | grep "GB" | grep -v "DeployStudioRuntime" | awk '{print $4}'`
+GETDISK=(diskutil list | grep "GB" | grep -v "DeployStudioRuntime" | head -1 | awk '{print $NF}')
 echo "$GETDISK"
 
-diskutil partitionDisk "${GETDISK}" HFS+ Macintosh\ HD 100%
+diskutil partitionDisk "${GETDISK}" GPTFormat HFS+ Macintosh\ HD 100%
 
 exit 0
